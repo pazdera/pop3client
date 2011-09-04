076ebaa3e042c4fe621acb559730efcec588c915
Radek Pazdera <radek.pazdera@gmail.com>
2011-09-04 14:15:15 +0200

    initial: Socket adapter
    Added Socket class, which is a wrapper around BSD socket api.

---
+++ socket.cpp
@@ -0,0 +1,146 @@
+/**
+ *  IPK 1 - webclient
+ *  Author: Radek Pazdera (xpazde00@stud.fit.vutbr.cz)
+ *  Date: 13.03.2010
+ *
+ *  Description:
+ *    Socket class is abstraction of BSD socket API.
+ */
+
+#include "socket.h"
+#include "errors.h"
+
+#include <string>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <unistd.h>
+#include <string.h>
+
+
+Socket::Socket (std::string inputAddress, std::string inputPort)
+{
+    socketFileDescriptor = -1;
+
+    address = inputAddress;
+    port    = inputPort;
+
+    open();
+}
+
+void Socket::open()
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *resultPointer;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = PF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;
+
+    if (getaddrinfo(address.c_str(), port.c_str(), &hints, &result) != 0)
+    {
+        throw ConnectionError("Server not found");
+    }
+
+    for (resultPointer = result; resultPointer != NULL; resultPointer = resultPointer->ai_next)
+    {
+        socketFileDescriptor = socket(resultPointer->ai_family, resultPointer->ai_socktype, resultPointer->ai_protocol);
+        if (socketFileDescriptor == -1)
+        {
+            continue;
+        }
+
+        if (connect(socketFileDescriptor, resultPointer->ai_addr, resultPointer->ai_addrlen) != -1)
+        {
+            break;
+        }
+
+        ::close(socketFileDescriptor);
+    }
+
+    if (resultPointer == NULL)
+    {
+        throw ConnectionError("Cannot establish connection to the server");
+    }
+
+    ::freeaddrinfo(result);
+}
+
+Socket::~Socket()
+{
+    if (socketFileDescriptor > 0)
+    {
+        close();
+    }
+}
+
+void Socket::close()
+{
+    ::shutdown(socketFileDescriptor, SHUT_RDWR);
+    ::close(socketFileDescriptor);
+}
+
+size_t Socket::read(char* buffer, size_t size)
+{
+    ssize_t bytesRead = ::read(socketFileDescriptor, buffer, size);
+    if (bytesRead < 0)
+    {
+        throw IOError("Recieving error", "Unable to resolve data from remote host");
+    }
+
+    return bytesRead;
+}
+
+void Socket::write(std::string request)
+{
+    if (::write(socketFileDescriptor, request.c_str(),(request.length())) < 0)
+    {
+        throw IOError("Sending error", "Unable to send data to remote host");
+    }
+}
+
+void Socket::readAll(std::string *response)
+{
+    char buffer;
+
+    while (read(&buffer, sizeof(buffer)))
+    {
+        *response += buffer;
+    }
+}
+
+bool Socket::readCharacter(char* buffer)
+{
+    if (read(buffer, 1) > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Socket::readLine(std::string* line)
+{
+    char buffer[2] = "";
+    *line = "";
+
+    if (readCharacter(&(buffer[1])))
+    {
+        do
+        {
+            *line     += buffer[0]; // add char to string
+            buffer[0]  = buffer[1];
+        }
+        while (readCharacter(&(buffer[1])) &&
+               !(buffer[0] == '\r' && buffer[1] == '\n'));
+     
+        line->erase(0, 1);
+    }
+}
+
---
+++ socket.h
@@ -0,0 +1,82 @@
+/**
+ *  IPK 1 - webclient
+ *  Author: Radek Pazdera (xpazde00@stud.fit.vutbr.cz)
+ *  Date: 13.03.2010
+ *
+ *  Description:
+ *    Socket class is abstraction of BSD socket API.
+ */
+
+#ifndef _SOCKET__H
+#define _SOCKET__H
+
+#include <string>
+
+#include "errors.h"
+/**
+ * Socket Class
+ *
+ * Tries to enstabilish socket connection between you and remote host using
+ * TCP protocol. The class also offers interface for sending and recieving
+ * messages through the open socket.
+ *
+ */
+class Socket
+{
+    int socketFileDescriptor;
+    std::string address;
+    std::string port;
+
+    public:
+        Socket(std::string inputAddress, std::string inputPort);
+        ~Socket();
+
+        /** Just interface for read(2) */
+        size_t read(char* buffer, size_t size);
+
+        /** Send data to remote host */
+        void write(std::string request);
+
+        /** Reads everything available from socket to a std::string  */
+        void readAll(std::string* response);
+
+        /** Read until \r\n or the end */
+        void readLine(std::string* line);
+
+
+        /* Exceptions */
+        class ConnectionError;
+        class IOError;
+
+    private:
+        /** Tries to enstabilish connection between you and the remote host */
+        void open();
+        /** Terminate socket connection */
+        void close();
+
+        /** Reads one byte from the socket */
+        bool readCharacter(char* buffer);
+
+};
+
+class Socket::ConnectionError : public Error
+{
+    public:
+        ConnectionError(std::string const& cause)
+        {
+            problem = "Unable to connect";
+            reason  = cause;
+        }
+};
+
+class Socket::IOError : public Error
+{
+    public:
+        IOError(std::string const& issue, std::string const& cause)
+        {
+            problem = issue;
+            reason  = cause;
+        }
+};
+
+#endif
