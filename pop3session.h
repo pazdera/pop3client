c3a10a0ba5df7386d02128550c49d96341807cc9
Radek Pazdera <radek.pazdera@gmail.com>
2011-09-04 18:44:49 +0200

    pop3session: Modified interface

--- pop3session.h
+++ pop3session.h
@@ -5,13 +5,27 @@ class Pop3Session
     Socket* socket;
 
     public:
-        Pop3Session();
+        Pop3Session(std::string const& server, int const& port);
         ~Pop3Session();
-        
+
+        void authenticate(std::string const& username, std::string const& password);
+
+        void printMessageList();
+        void printMessage(int messageId);
+
+
     private:
+        struct ServerResponse;
+
         void open();
         void close();
 
-        void authenticate();
         
+};
+
+struct Pop3Session::ServerResponse
+{
+    bool status;
+    std::string statusMessage;
+    std::list<std::string> data;
 };
