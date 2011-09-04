6b814999f5f26d938cf9c3b9d6bbd728c01c22d1
Radek Pazdera <radek.pazdera@gmail.com>
2011-09-04 14:14:22 +0200

    initial: Added arguments parser
    Added CliArguments class for parsing arguments.

---
+++ cliarguments.cpp
@@ -0,0 +1,104 @@
+/**
+ *  @author Radek Pazdera (xpazde00@stud.fit.vutbr.cz)
+ *
+ *  CliArguments implementation.
+ */
+
+
+#include "cliarguments.h"
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+CliArguments::CliArguments()
+{}
+
+void CliArguments::parse(int argc, char **argv)
+{
+    int option = 0;
+
+    /* Defaults */
+    port = DEFAULT_PORT;
+    hostname = "";
+    username = "";
+    messageId = 0;
+
+    while ((option = getopt (argc, argv, "h:p:u:")) != -1)
+    {
+      switch (option)
+      {
+        case 'h': /* Hostname */
+          setHostname(optarg);
+          break;
+        case 'p': /* Port number */
+          setPort(optarg);
+          break;
+        case 'u': /* Username */
+          setUsername(optarg);
+          break;
+        case '?':
+          throw GetoptError();
+          break;
+      }
+    }
+
+    /* id argument */
+    if (optind < argc)
+    {
+        setMessageId(argv[optind]);
+    }
+
+    verifyMandatoryArguments();
+}
+
+void CliArguments::verifyMandatoryArguments() const
+{
+    if (hostname.length() <= 0)
+    {
+        throw MissingArgumentError("-h");
+    }
+
+    if (username.length() <= 0)
+    {
+        throw MissingArgumentError("-u");
+    }
+}
+
+int CliArguments::convertStringToInteger(std::string numberStoredInString)
+{
+  return static_cast<int>(atoi(numberStoredInString.c_str()));
+}
+
+void CliArguments::setPort(char* optarg)
+{
+    port = convertStringToInteger(std::string(optarg));
+
+    if (port < MIN_PORT_RANGE || port > MAX_PORT_RANGE)
+    {
+        throw ArgumentDomainError("-p", "Value out of range (1 ~ 65535)");
+    }
+}
+
+void CliArguments::setHostname(char* optarg)
+{
+    hostname = std::string(optarg);
+}
+
+void CliArguments::setUsername(char* optarg)
+{
+    username = std::string(optarg);
+}
+
+void CliArguments::setMessageId(char* optarg)
+{
+    messageId = convertStringToInteger(optarg);
+
+    if (messageId <= 0)
+    {
+        throw ArgumentDomainError("id", "Message id must be number greater than 0");
+    }
+}
---
+++ cliarguments.h
@@ -0,0 +1,82 @@
+/**
+ *  Author: Radek Pazdera (xpazde00@stud.fit.vutbr.cz)
+ *  Date: 10.10.2010
+ *
+ *  Description:
+ *    This class parses cli arguments and then suplies
+ *    abstract interface for accessing them.
+ */
+
+
+#ifndef _CLIARGUMENTS__H
+#define _CLIARGUMENTS__H
+
+#include <string>
+#include <unistd.h>
+
+#include "errors.h"
+
+class CliArguments
+{
+    private:
+      static const int  MIN_PORT_RANGE = 1;
+      static const int  MAX_PORT_RANGE = 65535;
+      static const int  DEFAULT_PORT   = 110;
+
+      int port;
+      std::string username;
+      std::string hostname;
+      int messageId;
+
+    public: /* Interface */
+        CliArguments();
+
+        void parse(int argc, char **argv);
+
+        int getPort() const { return port; }
+        std::string getUsername() const { return username; }
+        std::string getHostname() const { return hostname; }
+        int getMessageId() const { return messageId; }
+
+    private:
+        static int convertStringToInteger(std::string numberStoredInString);
+
+        void setPort(char* optarg);
+        void setHostname(char* optarg);
+        void setUsername(char* optarg);
+        void setMessageId(char* optarg);
+
+        void verifyMandatoryArguments() const;
+
+    public: /* Exceptions */
+        class GetoptError;
+        class ArgumentDomainError;
+        class MissingArgumentError;
+};
+
+
+class CliArguments::GetoptError : public Error
+{};
+
+class CliArguments::ArgumentDomainError : public Error
+{
+    public:
+        ArgumentDomainError(std::string const& argument, std::string const& cause)
+        {
+            problem = "Invalid " + argument + " argument";
+            reason  = cause;
+        }
+};
+
+class CliArguments::MissingArgumentError : public Error
+{
+    public:
+        MissingArgumentError(std::string const& argument)
+        {
+            problem = "Missing argument";
+            reason  = argument + " is mandatory";
+        }
+};
+
+#endif
+
