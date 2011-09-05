fcf03ec12f3a9f4e337fb0245acc3ea1a4f0e5e1
Radek Pazdera <radek.pazdera@gmail.com>
2011-09-05 16:11:49 +0200

    Pop3Session: Command methods
    Implemented sending commands to pop3 server and recieving
    and parsing the response.

--- pop3session.cpp
+++ pop3session.cpp
@@ -21,4 +21,55 @@ Pop3Session::releaseSocket()
     }
 }
 
+void Pop3Session::sendCommand(std::string command)
+{
+    socket->write(command);
+}
+
+void Pop3Session::getResponse(ServerResponse* response)
+{
+    std::string buffer;
+    socket->readLine(&buffer);
+
+    if (buffer[0] == '+')
+    {
+        response->status = true;
+        buffer.erase(0, 3); // Remove the "+OK "
+    }
+    else
+    {
+        response->status = false;
+        buffer.erase(0, 4); // Remove the "-ERR "
+    }
+
+    response->statusMessage = buffer;
+    response->data.clear();
+}
+
+void Pop3Session::getMultilineResponse(ServerResponse* response)
+{
+    std::string buffer;
+    getResponse(response);
+
+    while (true)
+    {
+        buffer.clear();
+        socket->readLine(buffer);
+        
+        if (buffer == "." || buffer.length() == 0)
+        {
+            break;
+        }
+        else
+        {
+            if (buffer[0] == '.') /* Strip byte stuffed characters. */
+            {
+                buffer.erase(0, 1);
+            }
+
+            response->data.push_back(buffer);
+        }
+    }
+}
+
 
